284a3147d9c96cf37ef764bdc1a4a208e1dc74c2
GeorgeAWP <39963646+George-Rowland@users.noreply.github.com>
2022-05-09 09:33:44 +0100

    Oops lol

--- src/main/java/commands/FireCommand.java
+++ src/main/java/commands/FireCommand.java
@@ -26,9 +26,9 @@ public class FireCommand implements CommandExecutor {
                 }
                 else {
                     p.sendMessage(Color("&7You have set &c" + args[0] + " &7on fire!"));
-                    return true;
                     Player target = Bukkit.getPlayer(args[0]);
                     target.setFireTicks(60);
+                    return true;
                 }
             } else {
                 p.sendMessage(Color("&cYou do not have the permission to use this command (elements.fire)"));
